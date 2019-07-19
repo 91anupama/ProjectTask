bfeed2f579cfcfc18e73502449301743822f0359
Nilesh <Nilesh@Anu-pc.mshome.net>
2019-07-19 12:29:49 +0530

    *Dom* : document object model... Dom parser is used to create XML file.
    -construct Dom object with tree structure
    -write Dom object to a stream using Transformer and StreamResult
    Steps:
    1)Create documentBuilder instance n create documemt
    2) create Element class n appendChild method
    3) create new transformer instance n new DOMSourse instance
    4)  create a new StreamResult to output stream
    5)  use transform method to write Dom object to output
    *Sax* : use callback function to inform clients of the XML document
    structure
    -faster
    -use less memory
    -not load XML document in memory
    - not create object representation of XML
    *callback methods
    1) startDocument() , endDocument()
    2) startElement, endElement()
    3)  character()  : called between start and end tagpand

---
+++ DOMSAX_Task3/src/com/parser/DOM_CreateXML.java
@@ -0,0 +1,87 @@
+package com.parser;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class DOM_CreateXML {
+	public static final String xmlFilePath="F:\\Project_Task\\DOMSAX_Task3\\src\\com\\parser\\new.xml";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+			DocumentBuilderFactory documentFactory=DocumentBuilderFactory.newInstance();
+			DocumentBuilder documentBuilder=documentFactory.newDocumentBuilder();
+			Document document=documentBuilder.newDocument();
+			//root element
+			Element root=document.createElement("Company");
+			document.appendChild(root);
+			
+			//employee element
+			
+			Element employee=document.createElement("employee");
+			root.appendChild(employee);
+			
+			//set attribute to staff element
+			Attr attr=document.createAttribute("id");
+			attr.setValue("10");
+			employee.setAttributeNode(attr);
+			//employee.setAttribute("id", "10");
+			
+			//Firstname element
+			Element firstname=document.createElement("firstname");
+			firstname.appendChild(document.createTextNode("URVI"));
+			employee.appendChild(firstname);
+			
+			//lastname element
+			Element lastname=document.createElement("lastname");
+			lastname.appendChild(document.createTextNode("PANDIT"));
+			employee.appendChild(lastname);
+			
+			//email element
+			Element email=document.createElement("email");
+			email.appendChild(document.createTextNode("urvi@gmail.com"));
+			employee.appendChild(email);
+			
+			//department document
+			Element department=document.createElement("department");
+			department.appendChild(document.createTextNode("Human Resources"));
+			employee.appendChild(department);
+			//create the xml file
+			//transform the DOM object to an XML file
+			
+			TransformerFactory transformerfactory=TransformerFactory.newInstance();
+			Transformer transformer=transformerfactory.newTransformer();
+			DOMSource domSource = new DOMSource(document);
+            StreamResult streamResult = new StreamResult(new File(xmlFilePath));
+            transformer.transform(domSource, streamResult);
+            
+            System.out.println("Creating XML File using DOM");
+ 
+			
+			
+			
+			
+		} catch (ParserConfigurationException | TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+
+	}
+
+}
---
+++ DOMSAX_Task3/src/com/parser/SAXReadXML.java
@@ -0,0 +1,81 @@
+package com.parser;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class SAXReadXML {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			SAXParserFactory factory=SAXParserFactory.newInstance();
+			SAXParser saxParser=factory.newSAXParser();
+			DefaultHandler handler=new DefaultHandler() {
+				boolean bfname=false;
+				boolean blname=false;
+				boolean bemail=false;
+				boolean bdepartment=false;
+				String id;
+				boolean bemp;
+				Employee emp;
+			public void startElement(String s, String s1, String elementName, Attributes attributes) throws SAXException {
+				System.out.println("Start Element:"+elementName);
+				if(elementName.equalsIgnoreCase("employee")) {
+					emp=new Employee();
+					emp.setId(attributes.getValue("id"));
+					bemp=true;
+				}
+				if(elementName.equalsIgnoreCase("firstname")) {
+					bfname=true;
+				}
+				if(elementName.equalsIgnoreCase("lastname")) {
+					blname=true;
+				}
+				if(elementName.equalsIgnoreCase("email")) {
+					bemail=true;
+				}
+				if(elementName.equalsIgnoreCase("department")) {
+					bdepartment=true;
+				}
+			}
+			
+			public void endElement(String s, String s1, String elementName)throws SAXException{
+				System.out.println("End Element : "+elementName);
+			}
+			public void characters(char ch[], int start, int length)throws SAXException{
+				if (bemp) {
+					System.out.println("Employee : " + new String(ch, start, length));
+					emp.setId(id);
+					bemp = false;
+				}
+	
+				if (bfname) {
+					System.out.println("First Name : " + new String(ch, start, length));
+					bfname = false;
+				}
+				if (blname) {
+					System.out.println("Last Name : " + new String(ch, start, length));
+					blname = false;
+				}
+				if (bemail) {
+					System.out.println("Email : " + new String(ch, start, length));
+					bemail = false;
+				}
+				if (bdepartment) {
+					System.out.println("Department Name : " + new String(ch, start, length));
+					bdepartment = false;
+				}
+			}
+			};
+			saxParser.parse("F:\\Project_Task\\DOMSAX_Task3\\src\\com\\parser\\new.xml", handler);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
